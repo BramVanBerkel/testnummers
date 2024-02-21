41da9dcfe979b4a28a4efbd858a15c5940d88428
Bram van Berkel <bramvanberkel.prive@gmail.com>
2024-02-21 23:00:16 +0100

    remove format

--- src/components/generators/Timestamp.vue
+++ src/components/generators/Timestamp.vue
@@ -139,10 +139,6 @@ watch(state.date, () => updateLocaleString())
   <table class="min-w-full divide-y divide-gray-300 table-fixed">
     <tbody class="divide-y divide-gray-200">
     <tr>
-      <td class="w-32 py-4 pl-4 pr-3 text-sm font-medium text-gray-900">Format</td>
-      <td class="py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-0">Seconds</td>
-    </tr>
-    <tr>
       <td class="py-4 pl-4 pr-3 text-sm font-medium text-gray-900">Local time</td>
       <td class="py-4 pl-4 pr-3 text-sm text-gray-900 sm:pl-0"
           v-html="state.localeString.value"></td>
