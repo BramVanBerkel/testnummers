1ab74465817cb185c7e1a4c0ce45c5a566e5231e
Bram van Berkel <bramvanberkel.prive@gmail.com>
2024-02-21 22:52:24 +0100

    add unix timestamp

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import {createI18n} from "vue-i18n";
-
-const messages = {
-    nl: {
-        'copy': 'Kopieer',
-        'copied': 'Gekopieerd'
-    },
-}
-
-const i18n = createI18n({
-    locale: 'nl',
-    messages
-})
 
 const app = createApp(App);
 
-app.use(i18n);
-app.mount('#app')
+app.mount('#app');
