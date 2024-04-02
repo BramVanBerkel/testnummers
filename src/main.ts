4d911a9051b88b5802ed9df4b32a28a17b16e698
Bram van Berkel <bramvanberkel.prive@gmail.com>
2024-04-02 17:26:19 +0200

    add primevue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import { type Component, createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import InputText from 'primevue/inputtext'
+import InputGroup from 'primevue/inputgroup'
+import InputGroupAddon from 'primevue/inputgroupaddon'
+import InputNumber from 'primevue/inputnumber'
+import Button from 'primevue/button'
+import Dropdown from 'primevue/dropdown'
+import Lara from './presets/lara'
 
 const app = createApp(App as Component)
-
+app.component('InputText', InputText)
+app.component('InputGroup', InputGroup)
+app.component('InputGroupAddon', InputGroupAddon)
+app.component('InputNumber', InputNumber)
+app.component('Button', Button)
+app.component('Dropdown', Dropdown)
+app.use(PrimeVue, { unstyled: true, pt: Lara })
 app.mount('#app')
