bbe5ad21ac821f84aa5ec2b9bd23134a66ec7063
bramvanberkel <bramvanberkel.prive@gmail.com>
2025-08-29 14:42:35 +0200

    update password strength check to bruteforce time

--- src/generators/PasswordGenerator.ts
+++ src/generators/PasswordGenerator.ts
@@ -1,3 +1,13 @@
+export interface PasswordScore {
+  estimatedCrackTime: string
+  severity: string
+}
+
+interface GeneratedPassword {
+  password: string
+  score: PasswordScore
+}
+
 enum CharacterSet {
   UppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
   LowercaseLetters = 'abcdefghijklmnopqrstuvwxyz',
@@ -5,7 +15,20 @@ enum CharacterSet {
   Symbols = '^*%$!&@#'
 }
 
-export function generatePassword (length: number, lowercase: boolean, uppercase: boolean, numbers: boolean, symbols: boolean): string {
+/**
+ * average amount of hash/s for 12 RTX 5090's hashing bcrypt(10)
+ * based on research from https://www.hivesystems.com/password-table
+ */
+const DEFAULT_ATTEMPTS_PER_SECOND = 3732000n
+
+export function generatePasswordAndScore (length: number, lowercase: boolean, uppercase: boolean, numbers: boolean, symbols: boolean): GeneratedPassword {
+  return {
+    password: generatePassword(length, lowercase, uppercase, numbers, symbols),
+    score: generateScore(length, lowercase, uppercase, numbers, symbols)
+  }
+}
+
+function generatePassword (length: number, lowercase: boolean, uppercase: boolean, numbers: boolean, symbols: boolean): string {
   let characters = ''
   if (lowercase) characters += CharacterSet.LowercaseLetters
   if (uppercase) characters += CharacterSet.UppercaseLetters
@@ -23,4 +46,58 @@ export function generatePassword (length: number, lowercase: boolean, uppercase:
   }
 
   return password
+}
+
+function generateScore (length: number, lowercase: boolean, uppercase: boolean, numbers: boolean, symbols: boolean): PasswordScore {
+  const charsetSizes = {
+    UppercaseLetters: CharacterSet.UppercaseLetters.length,
+    LowercaseLetters: CharacterSet.LowercaseLetters.length,
+    Numbers: CharacterSet.Numbers.length,
+    Symbols: CharacterSet.Symbols.length
+  }
+
+  let charsetSize = 0
+  if (numbers) charsetSize += charsetSizes.Numbers
+  if (lowercase) charsetSize += charsetSizes.LowercaseLetters
+  if (uppercase) charsetSize += charsetSizes.UppercaseLetters
+  if (symbols) charsetSize += charsetSizes.Symbols
+
+  const totalCombinations = BigInt(charsetSize) ** BigInt(length)
+
+  const seconds = totalCombinations / DEFAULT_ATTEMPTS_PER_SECOND
+
+  const sec = 1n
+  const min = 60n * sec
+  const hour = 60n * min
+  const day = 24n * hour
+  const year = 365n * day
+  const thousand = 1000n
+  const million = thousand * thousand
+  const billion = thousand * million
+  const trillion = thousand * billion
+  const quadrillion = thousand * trillion
+  const quintillion = thousand * quadrillion
+
+  let formattedSeconds = ''
+  if (seconds < min) formattedSeconds = `${seconds} seconds`
+  else if (seconds < hour) formattedSeconds = `${(seconds / min)} minutes`
+  else if (seconds < day) formattedSeconds = `${(seconds / hour)} hours`
+  else if (seconds < year) formattedSeconds = `${(seconds / day)} days`
+  else if (seconds < thousand * year) formattedSeconds = `${(seconds / year)} years`
+  else if (seconds < million * year) formattedSeconds = `${(seconds / (thousand * year))}k years`
+  else if (seconds < billion * year) formattedSeconds = `${(seconds / (million * year))}m years`
+  else if (seconds < trillion * year) formattedSeconds = `${(seconds / (billion * year))}bn years`
+  else if (seconds < quadrillion * year) formattedSeconds = `${(seconds / (trillion * year))}tn years`
+  else if (seconds < quintillion * year) formattedSeconds = `${(seconds / (quadrillion * year))}qd years`
+  else formattedSeconds = `${(seconds / (quintillion * year))}qn years`
+
+  let severity = ''
+  if (seconds < year) severity = 'error'
+  else if (seconds < million * year) severity = 'warn'
+  else severity = 'success'
+
+  return {
+    estimatedCrackTime: formattedSeconds,
+    severity
+  }
 }
